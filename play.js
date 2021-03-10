312ce8280b53dfdaa11e70e3a00692a7b7b37b17
Devin MacGillivray <devhmac@gmail.com>
2021-03-10 21:28:24 +0000

    adding ctrl c as close parameter

--- play.js
+++ play.js
@@ -3,5 +3,27 @@ const net = require('net');
 //connect function from client.js
 const connect = require('./client');
 
+//setting up stdin to handle user input
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  // on key press close program
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      console.log("Thanks for using me, ciao!")
+      process.exit();
+    }
+  })
+
+  return stdin;
+}
+
+
+
+
+
 console.log('connecting....')
 connect();
+setupInput();
