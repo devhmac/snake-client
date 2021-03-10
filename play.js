894cb669c088e0407176efb5d86df3ea04012eef
Devin MacGillivray <devhmac@gmail.com>
2021-03-10 20:03:25 +0000

    moved client side code from play to client, exported connect() to play.js

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
 //requiring net package from node
 const net = require('net');
+const connect = require('./client');
 
-//
-const connect = () => {
-  // this is where we're connecting
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  //interpret incoming data as text(utf8)
-  conn.setEncoding('utf8')
-
-  // this is the event handler for incoming data from server to client
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  })
-
-  return conn;
-}
-
-console.log('Connecting .....')
+console.log('we connecting....')
 connect();
-
